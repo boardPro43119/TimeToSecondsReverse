35b4371a4feea988f264f4e940770a4623003c93
Benjamin Prud'homme <bgprudhomme@gmail.com>
2015-11-21 16:36:41 -0500

    Change from linear time to constant time

--- TimeToSecondsReverse.java
+++ TimeToSecondsReverse.java
@@ -2,18 +2,14 @@ import java.util.Scanner;
 public class TimeToSecondsReverse{
 	public static void main(String[] args){
 		Scanner in = new Scanner(System.in);
-		int totalHours=0, totalMinutes=0;
-		double totalSeconds;
+		long totalHours=0, totalMinutes=0;
 		System.out.println("Enter a time duration in seconds.  It will be converted to a combination of hours, minutes, and seconds.");
 		System.out.print("Seconds: ");
-		double seconds = in.nextDouble();
-		for(; seconds>=3600; seconds-=3600){
-			totalHours++;
-		}
-		for(; seconds>=60; seconds-=60){
-			totalMinutes++;
-		}
-		totalSeconds=seconds;
-		System.out.println("Total time: " + totalHours + " hours, " + totalMinutes + " minutes, " + totalSeconds + " seconds");
+		double seconds = in.nextDouble(); //take seconds from user
+		totalHours = (long) seconds/3600; //find number of hours in seconds
+		seconds-= totalHours*3600; //subtract seconds that are part of these hours
+		totalMinutes = (long) seconds/60; //find number of minutes in remaining seconds
+		seconds-= totalMinutes*60; //subtract seconds that are part of these minutes
+		System.out.println("Total time: " + totalHours + " hours, " + totalMinutes + " minutes, " + seconds + " seconds");
 	}
 }
